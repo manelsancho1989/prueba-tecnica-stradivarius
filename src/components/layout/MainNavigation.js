d271844c9d8913af41a686aefab1c248d9777d03
unknown <msancho.akx@gmail.com>
2022-05-10 20:54:13 +0200

    header sticky

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -15,8 +15,23 @@ export default function MainNavigation() {
 
   const countFavoriteMeetUps = useSelector(state => state.meetups.favorites).length;
 
+  let lastScrollTop = 0;
+
+  document.addEventListener("scroll", () => {
+    var header = document.getElementById('header-sticky');
+    let st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+      // downscroll code
+      header.style.top = '-100%';
+    } else {
+      // upscroll code
+      header.style.top = '0';
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+  }, false);
+
   return (
-    <header className={classes.header} data-test="navigation-header">
+    <header className={classes.header} data-test="navigation-header" id="header-sticky">
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
